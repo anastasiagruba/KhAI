d778ebe5db88d56ad5d0a8a387020ba50a4f2885
anastasiagruba <a.gruba@student.csn.khai.edu>
2021-12-08 22:59:55 +0200

    some commit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
  
-QPushButton button("Hello, everybody! What is up?");
+QPushButton button("Hello, everybody!");
 button.resize(180, 80);
 button.show( );
  
